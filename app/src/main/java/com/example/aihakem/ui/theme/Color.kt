1ccb5b94acfdc4e43fb49ed6b12d101071029ecb
ByteChester <mehmetalidokuyucu32@gmail.com>
2026-08-17 11:16:54 +0300

    Create Color.kt

---
+++ app/src/main/java/com/example/aihakem/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.aihakem.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Gruvbox Dark Renk Paleti
+val GruvboxBg = Color(0xFF282828)
+val GruvboxBgSoft = Color(0xFF3C3836)
+val GruvboxFg = Color(0xFFEBDBB2)
+val GruvboxGreen = Color(0xFFB8BB26)
+val GruvboxOrange = Color(0xFFFE8019)
+val GruvboxYellow = Color(0xFFFABD2F)
+val GruvboxGray = Color(0xFF928374)
